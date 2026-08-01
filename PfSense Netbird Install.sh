4c3daea3502c339927475c1928e87f8354d91c14
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2026-08-01 00:22:12 -0500

    Delete PfSense Netbird Install.sh

--- PfSense Netbird Install.sh
+++
@@ -1,6 +0,0 @@
-# https://github.com/netbirdio/pfsense-netbird/
-
-fetch https://github.com/netbirdio/pfsense-netbird/releases/download/v0.1.19/netbird-0.65.3-x86_64.pkg
-fetch https://github.com/netbirdio/pfsense-netbird/releases/download/v0.1.19/pfSense-pkg-NetBird-0.2.1-x86_64.pkg
-pkg add -f netbird-0.65.3-x86_64.pkg
-pkg add -f pfSense-pkg-NetBird-0.2.1-x86_64.pkg
